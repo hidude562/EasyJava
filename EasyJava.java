dae3ba3730bb9f34758a94f69e517d9a558c683e
Nathan M <82677882+hidude562@users.noreply.github.com>
2022-09-18 16:06:55 -0400

    Added simple string manipulation

--- EasyJava.java
+++ EasyJava.java
@@ -2,4 +2,21 @@ public class EasyJava {
    public static String getLastLetter(String input) {
       return input.substring(input.length() - 1);
    }
+   public static String getLastHalf(String input) {
+      return input.substring(input.length() / 2);
+   }
+   public static String getFirstHalf(String input) {
+      return input.substring(0, input.length() / 2);
+   }
+   public static String getFirstLetter(String input) {
+      return input.substring(0, 1);
+   }
+   public static String getReversedString(String input) {
+      String newString = "";
+      
+      for(int i = input.length() - 1; i >= 0; i--) {
+         newString += input.substring(i, i + 1);
+      }
+      return newString;
+   }
 }

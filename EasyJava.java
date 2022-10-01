4dbd3b91300402d75d8f380d7135aff3104a94c9
Nathan M <82677882+hidude562@users.noreply.github.com>
2022-10-01 11:39:57 -0400

    Add input

--- EasyJava.java
+++ EasyJava.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class EasyJava {
+   
+   
+   /* String manipulation */
+   
    public static String getLastLetter(String input) {
       return input.substring(input.length() - 1);
    }
@@ -21,5 +27,27 @@ public class EasyJava {
    }
    public static int getRandom(int start, int stop) {
       return (int) ((Math.random() * (stop - start)) + start + 0.5);
+   }
+   
+   
+   /* User input methods */
+   
+   public static String getInputString(String prompt) {
+      Scanner input = new Scanner(System.in);
+      System.out.println(prompt);
+      
+      return input.nextLine();
+   }
+   public static String getInputInt(String prompt) {
+      Scanner input = new Scanner(System.in);
+      System.out.println(prompt);
+      
+      return input.nextInt();
+   }
+   public static String getInputDouble(String prompt) {
+      Scanner input = new Scanner(System.in);
+      System.out.println(prompt);
+      
+      return input.nextDouble();
    }
 }
